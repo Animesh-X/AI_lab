b1b3e804519bd2d76a4cfaee4bbe4608e0ce9d74
Animesh-X <animeshkumar0715@gmail.com>
2024-02-07 11:59:06 +0530

    evaluation_assignment_1

---
+++ Evaluation_Assignments/Assignment_1/N_Puzzle_Problem.py
@@ -0,0 +1,123 @@
+import copy
+class Node():
+    def __init__(self, state, parent):
+        self.state = state
+        self.parent = parent
+        
+
+class QueueFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+    
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+
+print(f"Enter the Initial State(3x3) (-1 for blank space): ")
+
+initial_state = []
+for i in range(3):
+    initial_state.append(list(map(lambda x: int(x) ,input().split())))
+
+
+
+print(f"Enter the Goal State(3x3) (-1 for blank space): ")
+
+goal_state = []
+for i in range(3):
+    goal_state.append(list(map(lambda x: int(x) ,input().split())))
+
+
+def result_after_move(current, position, next_move):
+    x,y = position
+    i,j = next_move
+    current_copy = copy.deepcopy(current)
+    if i<0 or i>2 or j<0 or j>2 :
+        return None
+    else:
+        temp = current_copy[x][y]
+        current_copy[x][y] = current_copy[i][j]
+        current_copy[i] [j]=temp
+        return current_copy
+
+def possible_moves(current_state):
+    curr_X = 0
+    curr_Y = 0
+    possible_states = []
+    for i in range(3):
+        for j in range(3):
+            if current_state[i][j] == -1:
+                curr_X = i
+                curr_Y = j
+                break
+    delta_row = [0, -1, 0, 1]
+    delta_col = [-1, 0, 1, 0]
+    for i in range(4):
+        possible_X = curr_X + delta_row[i]
+        possible_Y = curr_Y + delta_col[i]
+        result = result_after_move(current_state, (curr_X, curr_Y), (possible_X, possible_Y))
+        if result is not None:
+            possible_states.append(result)
+    return possible_states    
+                
+
+def solve(start_state, end_state):
+    start = Node(state=start_state, parent=None)
+    
+    frontier = QueueFrontier()
+    frontier.add(start)
+    
+    explored = set()
+    
+    while True:
+        print('Hello')
+        if frontier.empty():
+            return None
+        
+        node = frontier.remove()
+        
+        if node.state == end_state:
+            solution = []
+            while node.parent is not None:
+                solution.append(node.state)
+                node = node.parent
+            solution.reverse()
+            return solution
+        
+        explored.add(tuple(map(tuple, node.state)))
+        
+        for state in possible_moves(node.state):
+            state_tuple = tuple(map(tuple, state))
+            if not frontier.contains_state(state) and state_tuple not in explored:
+                print('hi')
+                child = Node(state=state, parent=node)
+                frontier.add(child)
+
+
+solutions = solve(initial_state, goal_state)
+if solutions is None:
+    print("No solution exists for this start state: \n", initial_state, "\nand end state: \n", goal_state, "\n")
+else:
+    print("Starting State: \n")
+    for k in initial_state:
+        print(k)
+    print()
+    for i in solutions:
+        for j in i:
+            print(j)
+        print()
+    print("Goal State Reached!!!!\n")
+    
---
+++ Evaluation_Assignments/Assignment_1/q1.py
@@ -0,0 +1,45 @@
+import numpy as np
+import time
+
+m = int(input(f"Enter no of row of 1st matrix: "))
+n = int(input(f"Enter no of col of 1st matrix: "))
+
+p = int(input(f"Enter no of row of 2nd matrix: "))
+q = int(input(f"Enter no of col of 2nd matrix: "))
+
+start = time.process_time()
+matrixA = np.random.normal(loc = 0, scale = 1, size = (m,n))
+matrixB = np.random.uniform(low=0, high=1, size=(p, q))
+end = time.process_time()
+
+
+
+print("\nMatrix A:\n",matrixA,"\n")
+print("Matrix B:\n",matrixB,"\n")
+print("Time taken to create Matrices", (end - start) * 10**3, "ms.")
+
+start = time.process_time()
+try:
+    inverse_matrixA = np.linalg.inv(matrixA)
+    inverse_matrixB = np.linalg.inv(matrixB)
+except:
+    raise Exception("Cannot Inverse non-square Matrix")
+else:
+    print("\nInverse of  Matrix A is:\n",inverse_matrixA,"\n")
+    print("Inverse of Matrix B is:\n",inverse_matrixB,"\n")
+end = time.process_time()
+print("Time taken to Inverse Matrices", (end - start) * 10**3, "ms.")
+
+start = time.process_time()
+try:
+    product_inverse = np.dot(inverse_matrixA, inverse_matrixB)
+except:
+    raise Exception("Error Multiplying the Matrices")
+else:
+    print("\nProduct of Inverses:\n",product_inverse)
+end = time.process_time()
+print("\nTime taken to Multiply Matrices", (end - start) * 10**3, "ms.")
+
+
+
+
